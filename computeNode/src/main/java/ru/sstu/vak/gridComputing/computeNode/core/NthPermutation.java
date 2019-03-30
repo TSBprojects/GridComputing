22e861ce3888060846b3de487aa07ea93af1ac7f
Anton V <anton.v6@mail.ru>
2019-03-30 19:39:38 +0400

    Another code refactoring

--- computeNode/src/main/java/ru/sstu/vak/gridComputing/computeNode/core/NthPermutation.java
+++ computeNode/src/main/java/ru/sstu/vak/gridComputing/computeNode/core/NthPermutation.java
@@ -48,38 +48,6 @@ public class NthPermutation {
         return permutation;
     }
 
-    /**
-     * @param nodesCount количество всех узлов
-     * @param routeIndex номер маршрута
-     * @return перестановка по номеру {@code routeIndex} длиной {@code routeLength}
-     * из всех узлов {@code nodesCount}
-     * @deprecated вызов метода {@link NthPermutation#createFactorialsArray(int, int)}
-     * каждую итерацию сильно ухудшает производительность.
-     * Лучше использовать {@link NthPermutation#getPermutation(BigInteger)}
-     */
-    @Deprecated
-    public int[] getPermutation(int nodesCount, BigInteger routeIndex) {
-
-        int[] permutation = new int[routeLength];
-        BigInteger[] factorials = createFactorialsArray(nodesCount, routeLength);
-
-        for (int i = 0; i < routeLength; i++) {
-            BigInteger part = factorials[i];
-            permutation[i] = routeIndex.divide(part).intValue();
-            routeIndex = routeIndex.mod(part);
-        }
-
-        for (int i = routeLength - 1; i > 0; i--) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (permutation[j] <= permutation[i]) {
-                    permutation[i]++;
-                }
-            }
-        }
-
-        return permutation;
-    }
-
     private BigInteger[] createFactorialsArray(int nodesCount, int routeLength) {
         BigInteger[] factorials = new BigInteger[routeLength];
 
